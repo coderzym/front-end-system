ca410fef6fef5471aed0acdc0dbfbbe0cb8a895f
coderzym <zhaoyiming035@gmail.com>
2020-05-30 08:57:58 +0800

    更新部分注释

--- 前端实战/自定义Promise/ES6.js
+++ 前端实战/自定义Promise/ES6.js
@@ -6,12 +6,14 @@ class Promise {
         }
         // 初始化参数
         this.init()
+        // 为了防止Promise执行出错，所以我们要将它放在try...catch里面
         try {
             executor(this.resolve, this.reject)
         } catch (error) {
             this.reject(error)
         }
     }
+    // 每次使用Promise时需要初始化里面所有的状态、回调数组以及结果值(成功的是value，失败的是reason)
     init() {
         this.state = 'pending'
         this.resolve = this.resolve.bind(this)
@@ -21,6 +23,7 @@ class Promise {
         this.onResolvedCb = []
         this.onRejectedCb = []
     }
+    // resolve方法会清空onResolvedCb队列
     resolve(value) {
         if (this.state === 'pending') {
             this.state = 'resolved'
@@ -30,6 +33,7 @@ class Promise {
             })
         }
     }
+    // reject方法会清空onRejectedCb队列
     reject(reason) {
         if (this.state === 'pending') {
             this.state = 'rejected'
@@ -39,27 +43,40 @@ class Promise {
             })
         }
     }
+    // then方法会传入成功的回调和失败的回调
     then(onResolved, onRejected) {
+        // 如果传入的不是方法，那么返回这个原始值
         if (typeof onResolved !== 'function') {
             onResolved = function (value) {
                 return value
             }
         }
+        // 这里和上面的一致
         if (typeof onRejected !== 'function') {
             onRejected = function (reason) {
                 throw reason
             }
         }
+        // 根据Promise A+的规范里讲的，如果then的返回值依然是一个Promise对象，那么得分情况进行处理
+        // 这个过程可以理解为Promise的解决过程，它需要4个参数：
+        // 1.当前返回的Promise对象 2.被解决的值 3.成功的回调 4.失败的回调
+        // 规范上说：
+        //     如果 x 处于等待态， promise 需保持为等待态直至 x 被执行或拒绝
+        //     如果 x 处于执行态，用相同的值执行 promise
+        //     如果 x 处于拒绝态，用相同的据因拒绝 promise
         var promise2 = new Promise((resolve, reject) => {
             if (this.state === 'resolved') {
+                // 所以这里需要传入当前的值给Promise
                 let x = onResolved(this.value)
                 resolvePromise(promise2, x, resolve, reject)
             }
             if (this.state === 'rejected') {
+                // 同上
                 let x = onRejected(this.reason)
                 resolvePromise(promise2, x, resolve, reject)
             }
             if (this.state === 'pending') {
+                // 如果为等待态，那么得继续保持等待，所以传入回调数组中，等待下一次执行
                 this.onResolvedCb.push(() => {
                     let x = onResolved(this.value)
                     resolvePromise(promise2, x, resolve, reject)
@@ -117,8 +134,8 @@ class Promise {
         })
     }
     // 比如，catch((reason) => { console.log("catch: " + reason); throw "CATCH" })
-    // 所以这里的cb其实就是(reason) => { console.log("catch: " + reason); throw "CATCH" }，然后不传then的
-    // onResolve回调，只传个失败的
+    // 所以这里的cb其实就是(reason) => { console.log("catch: " + reason); throw "CATCH" }，
+    // 然后不传then的onResolve回调，只传个失败的
     catch(cb) {
         return this.then(null, cb)
     }
@@ -134,45 +151,79 @@ class Promise {
         )
     }
 }
-
+// 这里接上对Promise的处理，大家可以看看：
+// x 为对象或函数
+// 如果 x 为对象或者函数：
+//     把 x.then 赋值给 then
+//     如果取 x.then 的值时抛出错误 e ，则以 e 为据因拒绝 promise
+//     如果 then 是函数，将 x 作为函数的作用域 this 调用之。传递两个回调函数作为参数：
+//         1.第一个参数叫做 resolvePromise 
+//         2.第二个参数叫做 rejectPromise:
+//              如果 resolvePromise 以值 y 为参数被调用，则运行 [[Resolve]](promise, y)
+//              如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
+//           如果 resolvePromise 和 rejectPromise 均被调用，或者被同一参数调用了多次，
+//           则优先采用首次调用并忽略剩下的调用;
+//           如果调用 then 方法抛出了异常 e：
+//           如果 resolvePromise 或 rejectPromise 已经被调用，则忽略之
+//           否则以 e 为据因拒绝 promise
+//         如果 then 不是函数，以 x 为参数执行 promise
+//     如果 x 不为对象或者函数，以 x 为参数执行 promise
 function resolvePromise(promise2, x, resolve, reject) {
+    // promise2：当前返回的Promise实例
+    // x：结果值
+    // 后面2个是成功与失败的回调
+    // 设置called开关
     let called = false
+    // 如何Promise实例等于了这个结果值，那必须得退出，否则会进入死循环
     if (promise2 === x) {
         reject(new TypeError('Chaining cycle detected for promise'))
     }
+    // 如果结果值是Promise的实例
     if (x instanceof Promise) {
         x.then(
             value => {
+                // 成功的回调中继续使用递归
                 resolvePromise(promise2, value, resolve, reject)
             },
             reason => {
+                // 失败的话，那就失败咯
                 reject(reason)
             })
+    // 如果结果值为对象和函数并且它不为null
     } else if (x !== null && (typeof x === 'function' || typeof x === 'object')) {
+        // 那么就在try...catch中执行
         try {
+            // 将then方法拿出来
             let then = x.then
+            // 如果 then 是函数，将 x 作为函数的作用域 this 调用之。传递两个回调函数作为参数
             if (typeof then === 'function') {
                 then.call(x,
                     resolvePromise => {
+                        // 记住规范中说的：优先采用首次调用并忽略剩下的调用，
+                        // 所以我们最开始设置了called开关
                         if (called) return
                         called = true
                         resolvePromise(promise2, resolvePromise, resolve, reject)
                     },
                     rejectPromise => {
+                        // 同上
                         if (called) return
                         called = true
                         reject(rejectPromise)
                     })
             } else {
+                // 如果 then 不是函数，以 x 为参数执行 promise
                 if (called) return
                 called = true
                 resolve(x)
             }
+            // 否则以 e 为据因拒绝 promise
         } catch (error) {
             if (called) return
             called = true
             reject(error)
         }
+        // 如果 x 不为对象或者函数，以 x 为参数执行 promise
     } else {
         resolve(x)
     }
--- 前端实战/防抖/debounce.js
+++ 前端实战/防抖/debounce.js
@@ -7,20 +7,26 @@ function getUserAction() {
 
 container.onmousemove = debounce(getUserAction, 1000, true)
 
+// 接收三个参数，它们分别是回调、延迟时间、是否第一次立即执行
 function debounce(fn, wait, doNow) {
+    // 声明定时器容器，保存this指向，声明保存结果的容器
     var timeout, self = this, res
     function deBounced() {
+        // 如果是立即执行
         if (doNow) {
             res = fn.apply(self, arguments)
             doNow = false
         } else {
+            // 否则每次清除定时器
             clearTimeout(timeout)
+            // 在一定时间后执行
             timeout = setTimeout(() => {
                 res = fn.apply(self, arguments)
             }, wait);
         }
         return res
     }
+    // 提供清除节流的方法
     deBounced.cancel = function () {
         clearTimeout(timeout)
         timeout = null
