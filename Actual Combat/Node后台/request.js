46e836acffcb64c5c2ab0b34a63d3c0c646710e6
coderzym <1505789085@qq.com>
2021-07-26 15:45:48 +0800

    add prettierrc

--- Actual Combat/Node后台/request.js
+++ Actual Combat/Node后台/request.js
@@ -1,57 +1,57 @@
-const https = require('https')
+const https = require("https");
 
 function getRequest() {
   const options = {
-    hostname: 'nodejs.cn',
+    hostname: "nodejs.cn",
     port: 443,
-    path: '/todos',
-    method: 'GET'
-  }
-  
+    path: "/todos",
+    method: "GET",
+  };
+
   const req = https.request(options, res => {
-    console.log(`状态码: ${res.statusCode}`)
-  
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
-  })
-  
-  req.on('error', error => {
-    console.error(error)
-  })
-  
-  req.end()
+    console.log(`状态码: ${res.statusCode}`);
+
+    res.on("data", d => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on("error", error => {
+    console.error(error);
+  });
+
+  req.end();
 }
 
 function postRequest() {
   // PUT 和 DELETE 请求使用相同的 POST 请求格式，只需更改 options.method 的值即可。
   const data = JSON.stringify({
-    todo: '做点事情'
-  })
+    todo: "做点事情",
+  });
 
   const options = {
-    hostname: 'nodejs.cn',
+    hostname: "nodejs.cn",
     port: 443,
-    path: '/todos',
-    method: 'POST',
+    path: "/todos",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length
-    }
-  }
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },
+  };
 
   const req = https.request(options, res => {
-    console.log(`状态码: ${res.statusCode}`)
+    console.log(`状态码: ${res.statusCode}`);
 
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
-  })
+    res.on("data", d => {
+      process.stdout.write(d);
+    });
+  });
 
-  req.on('error', error => {
-    console.error(error)
-  })
+  req.on("error", error => {
+    console.error(error);
+  });
 
-  req.write(data)
-  req.end()
+  req.write(data);
+  req.end();
 }
--- Actual Combat/Node后台/startServer.js
+++ Actual Combat/Node后台/startServer.js
@@ -1,13 +1,14 @@
-const http = require('http')
+const http = require("http");
 
-const port = 3000
+const port = 3000;
+const hostname = "nodejs.cn";
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('你好世界\n')
-})
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("你好世界\n");
+});
 
 server.listen(port, () => {
-  console.log(`服务器运行在 http://${hostname}:${port}/`)
-})
+  console.log(`服务器运行在 http://${hostname}:${port}/`);
+});
