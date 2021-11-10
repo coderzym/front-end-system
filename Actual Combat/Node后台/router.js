1763063f2b13a91ffdba98c81d63abdd832f7c15
coderzym <1505789085@qq.com>
2021-11-10 21:03:09 +0800

    123

--- Actual Combat/Node后台/router.js
+++ Actual Combat/Node后台/router.js
@@ -4,7 +4,8 @@ function router(pathname, response) {
   console.log("????", pathname);
   fs.readFile(`./${pathname}.json`, "utf-8", (err, data) => {
     // response.write(`cb(${JSON.stringify(data)})`);
-    response.write(data.replace(/\r\n/g, ""));
+    // response.write(data.replace(/\r\n/g, ""));
+    response.write(`cb(1)`);
     response.end();
   });
 }
